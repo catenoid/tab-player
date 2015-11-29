f61c99d1d650f2b31e9fabfbca7ac3e229390ccd
Philippa Hack <philippa.hack@gmail.com>
2015-11-29 17:34:07 +0000

    Works multiple lines and sorts notes based on beat count

--- extract-notes.pl
+++ extract-notes.pl
@@ -1,19 +1,57 @@
 use warnings;
 use strict;
 
-my $tab = 'tab.txt';
-open(my $fh, '<:encoding(UTF-8)', $tab)
-  or die "Could not open file '$tab' $!";
+my $guitar_string_count = 6;
+
+my $infile = 'tab.txt';
+open(my $tab_fh, '<:encoding(UTF-8)', $infile)
+  or die "Could not open file '$infile' $!";
+
+my $outfile = 'notes.csv';
+open(my $notes_fh, '>', $outfile)
+  or die "Could not open file '$outfile' $!";
  
-while (my $row = <$fh>) {
+my $strings_seen = 0;
+my $stanza = 0;
+my @notes = ();
+my @sorted_notes;
+
+sub ordered_note {
+  # Sorting function returns -1 when $a precedes $b in the result
+  # $a and $b are array references
+  # Binary <=> returns -1 if left < right, 0 if equal, 1 otherwise
+  if ($a->[0] == $b->[0]) {
+    # Notes are in the same stanza
+    $a->[3] <=> $b->[3];
+  } else {
+  $a->[0] <=> $b->[0];
+  }
+}
+
+while (my $row = <$tab_fh>) {
+  my $last_string;
   if ($row =~ /(?<string>e|B|G|D|A|E)\|/) {
     my $beat_count = 0;
     my $string = $+{'string'};
     while ($' =~ /((?<note>\d+)|-)/) {
       if ((my $note = $+{'note'})) {
-        print "string: $string, note: $note, beat: $beat_count\n";
+        push @notes, [$stanza,$string,$note,$beat_count];
       }
       $beat_count += length($1);
     }
+    $strings_seen++;
+
+    $last_string = ($strings_seen == $guitar_string_count);
+    if ($last_string) {
+      $stanza++;
+      $strings_seen = 0;
+      @sorted_notes = sort ordered_note @notes;
+    }
   }
+}
+
+# Testing output
+my $i;
+for $i ( 0 .. $#sorted_notes ) {
+   print "stanza $sorted_notes[$i][0], beat $sorted_notes[$i][3]\n";
 }
