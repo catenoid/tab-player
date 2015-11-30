ac1073e6020cbfb5411f9b4e09ce31e8cd780b6e
Philippa Hack <philippa.hack@gmail.com>
2015-11-30 09:12:27 +0000

    Regex for fret matching accounts for concatenated single digits and symbols other than hyphens / digits.

--- extract-notes.pl
+++ extract-notes.pl
@@ -11,13 +11,6 @@ my $outfile = 'notes.csv';
 open(my $notes_fh, '>', $outfile)
   or die "Could not open file '$outfile' $!";
  
-sub ordered_note {
-  # Sorting function returns -1 when $a precedes $b in the result
-  # $a and $b are array references
-  # Binary <=> returns -1 if left < right, 0 if equal, 1 otherwise
-  $a->[2] <=> $b->[2];
-}
-
 # Parse the tab
 # Each stanza is composed of 6 rows representing the guitar strings
 my $guitar_string_count = 6;
@@ -27,14 +20,22 @@ my @sorted_notes;
 my $beat_offset = 0;
 my $is_last_string;
 
+sub ordered_note {
+  # Sorting function returns -1 when $a precedes $b in the result
+  # $a and $b are array references
+  # Binary <=> returns -1 if left < right, 0 if equal, 1 otherwise
+  $a->[2] <=> $b->[2];
+}
+
 while (my $row = <$tab_fh>) {
-  if ($row =~ /(?<string>E|B|G|D|A)\|/i) {
+  if ($row =~ /(E|B|G|D|A)[^-]*/i) {
     my $beat;
     my $beat_mantissa = 0;
-    while ($' =~ /(\d+|-)/) {
+    while ($' =~ /((?<fret>1[0-5]|[0-9])|-+|.)/) {
+      # Tabs can concatenate single-digit notes; Stop at fret 15
       $beat = $beat_offset + $beat_mantissa;
-      if ($1 ne '-') {
-        push @unsorted_notes, [$string, $1, $beat];
+      if (defined($+{'fret'})) {
+        push @unsorted_notes, [$string, $+{'fret'}, $beat];
       }
       $beat_mantissa += length($1);
     }
