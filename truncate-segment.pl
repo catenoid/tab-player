a09bba21bca21ffdc57ee7b86c190793ef715748
Philippa Hack <philippa.hack@gmail.com>
2015-12-04 14:59:35 +0000

    Selection filter fixed.

--- truncate-segment.pl
+++ truncate-segment.pl
@@ -29,15 +29,18 @@ my $last_string_index;
 FILTER: {
   for my $i (0 .. $#selection) {
     if ($selection[$i] =~ /^(E|A|D|G|B)/i) {
-      print "found first string ($1) is on line $i\n";
+      print "found first string ($1) is on line $i: $selection[$i]\n";
       $first_string_index = $i;
       for my $j ($i .. $#selection) {
         if ($selection[$j] !~ /^(E|A|D|G|B)/i) {
-          print "found last string ($1) is on line $j\n";
-          $last_string_index = $j;
+          print "found last string ($1) is on line ".($j-1)."\n";
+          $last_string_index = $j-1;
           last FILTER;
         }
       }
+      print "found last string on line $#selection: $selection[$#selection]\n";
+      $last_string_index = $#selection;
+      last FILTER;
     }
   }
 }
@@ -48,3 +51,19 @@ FILTER: {
 # Either:
 #  - Remove reference to string name, and modify extract-notes.pl to treat the first line of input as high e, OR
 #  - Prepend each line with the string name
+
+# what if you highlight a portion of one string, no labels at all?
+# say we need context
+
+my $incomplete_row_above = ($first_string_index != 0 and ($selection[$first_string_index-1] =~ /^(-|\d)+/)); 
+if ($incomplete_row_above) {
+  $first_string_index--;
+}
+
+my @strings = @selection[$first_string_index .. $last_string_index];
+
+for my $string (@strings) {
+  print "$string\n";
+}
+
+# check begin < end
