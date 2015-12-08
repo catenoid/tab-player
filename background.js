c80a0d2cf691ee2c8f5d7f17514a9b3b4e9993b6
catenoid <hackphilippa@gmail.com>
2015-12-08 12:58:09 +0000

    XHR added, stepping stone to mp3 playback
    Perl server responds with $c->send_file( example.mp3 ) to all requests.

--- background.js
+++ background.js
@@ -1,10 +1,12 @@
+var url = "http://192.168.56.101:8000/";
+
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 	switch(request.message) {
 		case 'setText':
 			console.debug("Received highlighted text:");
 			console.debug(request.data);
 			console.debug("Data from server:");
-			$.get("http://192.168.56.101:8000/", {"tab": request.data}, function(data) {console.debug(data);});
+			loadXHR(request.data, decode);
 		break;
 
 		default:
@@ -12,3 +14,22 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 		break;
 	}
 });
+
+function loadXHR(tab, callback) {
+  try {
+    var xhr = new XMLHttpRequest();
+	var url_params = url + "?tab=" + tab;
+    xhr.open("GET", url_params);
+    xhr.responseType = "arraybuffer";
+    xhr.onerror = function() {console.debug("Network error.")};
+    xhr.onload = function() {
+      if (xhr.status === 200) callback(xhr.response);
+      else console.debug("Loading error:" + xhr.statusText);
+    };
+    xhr.send();
+  } catch (err) {console.debug(err.message)}
+}
+
+function decode(buffer) {
+  console.debug("XHR Complete");
+}
