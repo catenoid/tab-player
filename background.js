aab42f79a0ce4cebbe58cf122a90eecdff88b7af
catenoid <hackphilippa@gmail.com>
2015-12-08 13:14:13 +0000

    Example mp3 plays

--- background.js
+++ background.js
@@ -1,4 +1,6 @@
 var url = "http://192.168.56.101:8000/";
+var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+var source;
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 	switch(request.message) {
@@ -6,7 +8,8 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 			console.debug("Received highlighted text:");
 			console.debug(request.data);
 			console.debug("Data from server:");
-			loadXHR(request.data, decode);
+			loadXHR(request.data);
+			source.start(0);
 		break;
 
 		default:
@@ -15,21 +18,23 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 	}
 });
 
-function loadXHR(tab, callback) {
+function loadXHR(tab) {
   try {
+    source = audioCtx.createBufferSource();
     var xhr = new XMLHttpRequest();
 	var url_params = url + "?tab=" + tab;
     xhr.open("GET", url_params);
     xhr.responseType = "arraybuffer";
     xhr.onerror = function() {console.debug("Network error.")};
     xhr.onload = function() {
-      if (xhr.status === 200) callback(xhr.response);
-      else console.debug("Loading error:" + xhr.statusText);
-    };
+		var audioData = xhr.response;
+		audioCtx.decodeAudioData(audioData, function(buffer) {
+			source.buffer = buffer;
+			source.connect(audioCtx.destination);
+			source.loop = false;
+			},
+		function(e){"Error with decoding audio data" + e.err});
+	}
     xhr.send();
   } catch (err) {console.debug(err.message)}
-}
-
-function decode(buffer) {
-  console.debug("XHR Complete");
 }
